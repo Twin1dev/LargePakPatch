35f524b3cfbdda14846c2352f0327d8dcdab14ac
Twin1 <zaydinortiz@gmail.com>
2024-01-11 11:09:34 -0600

    switch back to 9.10 offsets

--- LargePakPatch/Offsets.h
+++ LargePakPatch/Offsets.h
@@ -19,8 +19,8 @@
 namespace Offsets
 {
 	// Search RequestExit, last xref usually, f5, if the sub has postquitmessage in it, then you have the right one.
-	unsigned __int64 RequestExit = 0x1CED030;
+	unsigned __int64 RequestExit = 0x1E6CC40;
 
 	// Search UnsafeEnvironment_Title, there should only be one xref, f5, you have it.
-	unsigned __int64 UnsafeEnvironment = 0x15144A0;
+	unsigned __int64 UnsafeEnvironment = 0x163E3D0;
 }
