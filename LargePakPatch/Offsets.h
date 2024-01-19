e1ec7f26babe4cac8b0ef407a2fc7c688eabe70f
parkie <69488850+mlodyskiny@users.noreply.github.com>
2024-01-19 18:42:27 +0100

    no spoonfeed ue is opensource

--- LargePakPatch/Offsets.h
+++ LargePakPatch/Offsets.h
@@ -18,7 +18,6 @@
 
 namespace Offsets
 {
-	// Search RequestExit, last xref usually, f5, if the sub has postquitmessage in it, then you have the right one.
 	unsigned __int64 RequestExit = 0x1E6CC40;
 
 	// Search UnsafeEnvironment_Title, there should only be one xref, f5, you have it.
