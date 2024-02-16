043176f2f279afc36ae7d3df05e7daf9d7dd01fc
Twin1 <64933300+Twin1dev@users.noreply.github.com>
2024-02-15 20:31:44 -0600

    just make it one line

--- LargePakPatch/Hooks.cpp
+++ LargePakPatch/Hooks.cpp
@@ -3,14 +3,9 @@
 #include "minhook/MinHook.h"
 #include "Addresses.h"
 
-void __fastcall Hooks::RequestExitHk(unsigned __int8 a1)
-{
-}
+void __fastcall Hooks::RequestExitHk(unsigned __int8 a1) {}
 
-__int64 __fastcall Hooks::UnsafeEnvironmentHk(__int64* a1, char a2, __int64 a3, char a4)
-{
-	return 0;
-}
+__int64 __fastcall Hooks::UnsafeEnvironmentHk(__int64* a1, char a2, __int64 a3, char a4) { return 0; }
 
 bool Hooks::Create()
 {
