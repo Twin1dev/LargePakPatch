7fda5825ee5919457269e82ccf9645422ee60639
Twin1 <64933300+Twin1dev@users.noreply.github.com>
2024-04-02 22:40:43 -0500

    11.31 offsets
    (im sorry i havent done shit with this, im horrible at keeping ideas running)

--- LargePakPatch/Offsets.h
+++ LargePakPatch/Offsets.h
@@ -16,6 +16,12 @@
 		UnsafeEnvironment: 0x163E3D0
 */
 
+/*
+	Version: 11.31
+		RequestExit: 0x269CFB0
+		UnsafeEnvironment: 0x1BF07B0
+*/
+
 namespace Offsets
 {
 	unsigned __int64 RequestExit = 0x1E6CC40;
